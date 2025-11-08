14fc38cf80f31c06105c7325a78ab94ee42a7c08
Sajda Parveen <sajdak486@gmail.com>
2025-11-08 22:53:48 +0200

    practicee js

---
+++ Day19js/script.js
@@ -0,0 +1,15 @@
+console.log("hy");
+console.warn("becareful");
+console.error("u r wrong");
+console.info("this is information");
+console.table({name:"sajda"})
+let x = "sajda is a good girl";
+let y = x.slice(0,5);
+console.log(y);
+console.log(y.split());
+console.log(y.split(""));
+console.log(x.split())
+let z = "sajda behan ksi ho";
+console.log(z.replace("a","e"))   //it will only replace the 1st word
+console.log(z.replaceAll("a","e")) //it will replace all the replaceable value
+console.log(z.includes("f"))
