844602f784560a3813dd92ba418381ad3e972e38
Sajda Parveen <sajdak486@gmail.com>
2025-11-26 01:05:15 +0530

    basic paper generator

--- day24/script.js
+++ day24/script.js
@@ -1,9 +1,23 @@
 let btn = document.querySelector("button");
 let main = document.querySelector("main")
 btn.addEventListener("click",function(){
-    let h1 = document.createElement("h1");
-    h1.innerHTML = "Hello User";
-    h1.style.color = "white";
-    main.appendChild(h1);
+    let div = document.createElement("div")
+    let x = Math.random()*100;
+    console.log(x);
+    let y = Math.random()*100;
+    console.log(y)
+    let c1 = Math.floor(Math.random()*256);
+    let c2 = Math.floor(Math.random()*256);
+    let c3 = Math.floor(Math.random()*256);
+    let r = Math.floor(Math.random()*360)
+    div.style.height = "50px";
+    div.style.width = "50px";
+    div.style.position = "absolute";
+    div.style.left = `${x}%`;
+    div.style.top = `${y}%`;
+    div.style.rotate = `${r}deg`
+    div.style.backgroundColor = `rgb(${c1},${c2},${c3})`
+    
+    main.appendChild(div)
     
 })
