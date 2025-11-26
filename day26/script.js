02fefd4cac68257df6be73f6ce716dda66a3a3ed
Sajda Parveen <sajdak486@gmail.com>
2025-11-27 00:55:54 +0530

    mp3 Downloader practicing

---
+++ day26/script.js
@@ -0,0 +1,37 @@
+let btn = document.querySelector("button");
+let grow = 0;
+let h2 = document.querySelector("h2")
+let inner = document.querySelector(".inner")
+
+
+
+btn.addEventListener("click",()=>{
+    btn.style.pointerEvents = 'none';
+
+    let num = 50 + Math.floor(Math.random()*50);
+    console.log(num)
+
+
+
+
+   let int = setInterval(()=>{
+        grow++
+        h2.innerHTML = `${grow}%`;
+        inner.style.width = `${grow}% `
+
+
+    },num)
+
+    setTimeout(()=>{
+        clearInterval(int)
+        btn.innerHTML = "Downloaded";
+        btn.style.opacity = 0.5;
+
+         console.log("Downloaded in",num/10,"sec")
+
+    },num*100)
+
+
+   
+    
+})
